30816ff72a1cfc0f3777179caeb66d90b6b26555
youssef <youssef.elassaoui@gmail.com>
2025-03-20 11:08:35 +0000

    initial one

---
+++ src/main/java/monitoring/IntelligentpvApplication.java
@@ -0,0 +1,13 @@
+package monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentpvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentpvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/monitoring/IntelligentpvApplicationTests.java
@@ -0,0 +1,13 @@
+package monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentpvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
